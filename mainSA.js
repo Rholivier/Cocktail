25eea06fa0d08589176896c1ee746be19905e755
Rholivier <olivier.terral@sncf.fr>
2022-05-19 10:05:29 +0200

    final

--- mainSA.js
+++
@@ -1,8 +0,0 @@
-let voir = document.querySelector(".bg");
-let visu = document.querySelector("p");
-voir = addEventListener("click", () => {
-    if(getComputedStyle(visu).display != "none") {
-        visu.style.display = none;
-    } else {
-        visu.style.display = initial;
-    })
